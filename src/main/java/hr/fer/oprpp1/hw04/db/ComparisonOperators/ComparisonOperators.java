1cf6a1420145952152a4829077c9491a58e41dfb
Leo Goršić <gorsicleo@gmail.com>
2021-11-16 01:52:31 +0100

    Add comparison operators

---
+++ src/main/java/hr/fer/oprpp1/hw04/db/ComparisonOperators/ComparisonOperators.java
@@ -0,0 +1,42 @@
+package hr.fer.oprpp1.hw04.db.ComparisonOperators;
+
+
+import hr.fer.oprpp1.hw04.db.IComparisonOperator.IComparisonOperator;
+
+public class ComparisonOperators {
+
+	public static final IComparisonOperator LESS;
+	public static final IComparisonOperator LESS_OR_EQUALS;
+	public static final IComparisonOperator GREATER;
+	public static final IComparisonOperator GREATER_OR_EQUALS;
+	public static final IComparisonOperator EQUALS;
+	public static final IComparisonOperator NOT_EQUALS;
+	public static final IComparisonOperator LIKE;
+
+	static {
+		LESS = (first, second) ->  first.compareToIgnoreCase(second) == 1;
+		LESS_OR_EQUALS = (first, second) -> first.compareToIgnoreCase(second) >= 0;
+		GREATER = (first, second) -> first.compareToIgnoreCase(second) == -1;
+		GREATER_OR_EQUALS = (first, second) -> first.compareToIgnoreCase(second) <= 0;
+		EQUALS = (first,second) -> first.compareToIgnoreCase(second) == 0;
+		NOT_EQUALS = (first, second) -> first.compareToIgnoreCase(second) != 0;
+		LIKE = (statement, pattern) -> {
+			int starIndex = pattern.indexOf('*');
+			String endRequirement = pattern.substring(starIndex+1);
+			String startRequirement = pattern.substring(0, starIndex);
+			
+			if (endRequirement.isEmpty()) {
+				return statement.startsWith(startRequirement);
+			}
+			
+			if (startRequirement.isEmpty()) {
+				return statement.endsWith(endRequirement);
+			}
+			
+			return (statement.endsWith(endRequirement) && statement.startsWith(startRequirement)
+					&& startRequirement.length()+endRequirement.length()<=statement.length());
+				
+			
+		};
+	}
+}
